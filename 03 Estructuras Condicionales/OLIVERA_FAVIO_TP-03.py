36e12b2262c66956b6d72dd19a4d20994767e697
Favio10 <favio_olivera@hotmail.com>
2025-04-10 20:07:16 -0300

    TP3 ejecicio3

--- 03 Estructuras Condicionales/OLIVERA_FAVIO_TP-03.py
+++ 03 Estructuras Condicionales/OLIVERA_FAVIO_TP-03.py
@@ -21,12 +21,18 @@ elif(nota < 6 and nota >= 0):
     print("Desaprobado.")
 else:
     print("Nota inválida. Debe estar entre 0 y 10.")
-    
+
 
 """3. Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
    número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
    contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
    operador de módulo (%) en Python para evaluar si un número es par o impar."""
+
+numero = int(input("Ingresa un número para validar si es par: "))
+if(numero % 2 == 0):
+    print("Ha ingresado un número par")
+else:
+    print("Por favor, ingrese un número par")
 
 
 """4. Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
