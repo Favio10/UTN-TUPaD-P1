68604513a7c20a44cdc3bc35f8be2359fe9b8ca8
Favio10 <favio_olivera@hotmail.com>
2025-06-10 10:55:19 -0300

    tp5

--- 05 Funciones/OLIVERA_FAVIO_TP2.py
+++ 05 Funciones/OLIVERA_FAVIO_TP2.py
@@ -7,9 +7,14 @@ def imprimir_hola_mundo():
 def saludar_usuario(nombre):
     print(f"Hola {nombre}!")
 
+#3
+def informacion_personal(nombre, apellido, edad, residencia):
+    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
 
 
 
 #Programa principal
 
 imprimir_hola_mundo()
+saludar_usuario(input("Ingresá tu nombre: "))
+informacion_personal(input("Ingresa tu nombre: "), input("Ingresa tu apellido: "), int(input("Ingresa tu edad: ")), input("Ingresa tu lugar de residencia: "))
