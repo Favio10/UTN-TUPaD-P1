59fd5675b0a462442114a8aee8560947362517f7
Favio10 <favio_olivera@hotmail.com>
2025-06-10 11:07:41 -0300

    tp5

--- 05 Funciones/OLIVERA_FAVIO_TP2.py
+++ 05 Funciones/OLIVERA_FAVIO_TP2.py
@@ -11,6 +11,16 @@ def saludar_usuario(nombre):
 def informacion_personal(nombre, apellido, edad, residencia):
     print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
 
+#4
+def calcular_area_circulo(radio):
+    import math
+    area = math.pi * (radio ** 2)
+    print(f"El área del círculo con radio {radio} es: {area}")
+
+def calcular_perimetro_circulo(radio):
+    import math
+    perimetro = 2 * math.pi * radio
+    print(f"El perímetro del círculo con radio {radio} es: {perimetro}")
 
 
 #Programa principal
@@ -18,3 +28,5 @@ def informacion_personal(nombre, apellido, edad, residencia):
 imprimir_hola_mundo()
 saludar_usuario(input("Ingresá tu nombre: "))
 informacion_personal(input("Ingresa tu nombre: "), input("Ingresa tu apellido: "), int(input("Ingresa tu edad: ")), input("Ingresa tu lugar de residencia: "))
+calcular_area_circulo(float(input("Ingresa el radio del círculo para calcular el area: ")))
+calcular_perimetro_circulo(float(input("Ingresa el radio del círculo para calcular el perímetro: ")))
